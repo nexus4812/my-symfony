f2b84cdfe5159abaa3a9d62a0e2dc99256304177
nexus4812 <powerbatteryboy@gmail.com>
2023-07-17 21:29:56 +0900

    wip

--- src/Controller/CreateController.php
+++ src/Controller/CreateController.php
@@ -2,13 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\TaskRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[AsController]
 class CreateController
 {
+    public function __construct(private TaskRepository $repository)
+    {
+    }
+
     #[Route('/create')]
-    public function number(): Response
+    public function create(): Response
     {
         $number = random_int(0, 100);
 
